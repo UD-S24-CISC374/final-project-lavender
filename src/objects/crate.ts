b4ff4732853daabb7e80353f724f1c2e361c7a65
jsmtaing <jostai2003@gmail.com>
2024-04-25 14:50:30 -0400

    Add in frameMap and setIngredient function.

--- src/objects/crate.ts
+++ src/objects/crate.ts
@@ -1,18 +1,32 @@
 import Phaser from "phaser";
-//import { Ingredient } from "./dish_ing";
+import { Ingredient } from "./dish_ing";
 
 interface CrateProps {
     scene: Phaser.Scene;
     x: number;
     y: number;
+    ingredient: Ingredient;
 }
 
 export class Crate extends Phaser.Physics.Arcade.Sprite {
     //Frame of crate sprite determines what ingredient is stored within...
     //F0: banana, F1: blueberry, F2: bread, F3: butter, F4: eggs, F5: milk
+    static ingredientFrameMap: { [key: string]: number } = {
+        BA: 0,
+        BL: 1,
+        BR: 2,
+        BU: 3,
+        EG: 4,
+        MI: 5,
+    };
+
     constructor(config: CrateProps) {
         super(config.scene, config.x, config.y, "crates");
+        this.setIngredient(config.ingredient);
     }
 
-    setIngredient() {}
+    setIngredient(ingredient: Ingredient) {
+        const index = Crate.ingredientFrameMap[ingredient.name];
+        this.setFrame(index);
+    }
 }
