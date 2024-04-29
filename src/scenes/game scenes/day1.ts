b3cc218e0c9af10c93c57da9ec76a5cdb299f6d9
jsmtaing <jostai2003@gmail.com>
2024-04-29 12:09:39 -0400

    Fix levelSelect, re-add testing objects for day1

--- src/scenes/game scenes/day1.ts
+++ src/scenes/game scenes/day1.ts
@@ -1,6 +1,9 @@
 import Phaser from "phaser";
 import { Player } from "../../objects/player";
 import { Player_Arms } from "../../objects/player_arms";
+import { Ingredient } from "../../objects/dish_ing";
+import { Crate } from "../../objects/crate";
+import { Stove } from "../../objects/stove";
 
 export type Collidable =
     | Phaser.Types.Physics.Arcade.GameObjectWithBody
@@ -16,36 +19,60 @@ export default class day1 extends Phaser.Scene {
     private player_arms: Player_Arms;
     private cursors: Phaser.Input.Keyboard.KeyboardPlugin | null;
 
+    private stove: Stove;
     private itemGroup?: Phaser.Physics.Arcade.Group;
-    private heldItem: Phaser.Physics.Arcade.Sprite | null;
-
-    private popupVisible: boolean = true;
-    private textBackground: Phaser.GameObjects.Rectangle;
-    private instructions: Phaser.GameObjects.Text;
-
-    private secondPopupVisible: boolean = false;
-    private secondTextBackground: Phaser.GameObjects.Rectangle;
-    private secondInstructions: Phaser.GameObjects.Text;
-
-    private thirdTextBackground: Phaser.GameObjects.Rectangle;
-    private thirdInstructions: Phaser.GameObjects.Text;
-
-    private continueButton: Phaser.GameObjects.Text;
+    private crateGroup: Phaser.Physics.Arcade.Group;
+    private heldItem: Ingredient | null;
 
     create() {
         //Creates tile and map.
         const map = this.make.tilemap({ key: "map_1" });
-        const tileset = map.addTilesetImage("Room_Builder_48x48", "tiles"); //Tilemap name, then key preloader name
+        const tileset = map.addTilesetImage("Room_Builder_48x48", "tiles");
 
-        //Creates and randomizes tomato position.
+        //Creates stove object.
+        this.stove = new Stove({
+            scene: this,
+            x: this.cameras.main.displayWidth / 2 - 40,
+            y: this.cameras.main.displayHeight / 2 + 20,
+        });
+        this.stove.createAnims();
+
+        //Create itemgroup
         let x, y;
         const numOfObjects = 10;
         this.itemGroup = this.physics.add.group();
         for (let i = 0; i < numOfObjects; i++) {
-            x = Phaser.Math.RND.between(20, 1180);
-            y = Phaser.Math.RND.between(50, 700);
-            this.itemGroup.add(this.physics.add.sprite(x, y, "tomato"));
+            x = Phaser.Math.RND.between(30, 1170);
+            y = Phaser.Math.RND.between(60, 690);
+            this.itemGroup.add(
+                new Ingredient({ scene: this, x: x, y: y }, "banana").setScale(
+                    0.5
+                )
+            );
         }
+
+        //Create crateGroup
+        this.crateGroup = this.physics.add.group();
+        x = 80;
+        y = 150;
+        this.crateGroup.add(
+            new Crate({ scene: this, x: x, y: y, ingredient: "BA" })
+        );
+        this.crateGroup.add(
+            new Crate({ scene: this, x: x, y: y + 60, ingredient: "BL" })
+        );
+        this.crateGroup.add(
+            new Crate({ scene: this, x: x, y: y + 120, ingredient: "BR" })
+        );
+        this.crateGroup.add(
+            new Crate({ scene: this, x: x, y: y + 180, ingredient: "BU" })
+        );
+        this.crateGroup.add(
+            new Crate({ scene: this, x: x, y: y + 240, ingredient: "EG" })
+        );
+        this.crateGroup.add(
+            new Crate({ scene: this, x: x, y: y + 300, ingredient: "MI" })
+        );
 
         //Creates player input and player object.
         this.cursors = this.input.keyboard;
@@ -63,17 +90,37 @@ export default class day1 extends Phaser.Scene {
         });
         this.player_arms.createAnims();
 
+        //Add overlap between player_arms and stove.
+        this.physics.add.overlap(
+            this.player_arms,
+            this.stove,
+            (playerArms) => {
+                (playerArms as Player_Arms).stoveOverlap = true;
+            },
+            (playerArms) => {
+                return !(playerArms as Player_Arms).stoveOverlap;
+            },
+            this
+        );
         //Add overlap between player_arms and game objects.
         this.physics.add.overlap(
             this.player_arms,
             this.itemGroup,
             (playerArms, item) => {
                 (playerArms as Player_Arms).overlapping = true;
-                this.heldItem = item as Phaser.Physics.Arcade.Sprite;
+                this.heldItem = item as Ingredient;
             },
             (playerArms) => {
                 return !(playerArms as Player_Arms).hasItem;
             },
+            this
+        );
+        //Add overlap between player_arms and crates.
+        this.physics.add.overlap(
+            this.player_arms,
+            this.crateGroup,
+            undefined,
+            undefined,
             this
         );
 
@@ -96,41 +143,82 @@ export default class day1 extends Phaser.Scene {
         }
     }
 
+    //Helper functions
+    resetClicked() {
+        this.player_arms.overlapping = false;
+        this.player_arms.stoveOverlap = false;
+        if (!this.input.mousePointer.leftButtonDown()) {
+            this.mouseClicked = false;
+        }
+    }
+    pickupItem(pickingUp: boolean) {
+        if (pickingUp) {
+            this.player_arms.hasItem = true;
+            this.player_arms.playAnims(true);
+            this.heldItem?.setPosition(this.player.x, this.player.y - 50);
+            this.mouseClicked = true;
+        } else {
+            //Holding item
+            this.player_arms.playAnims(true);
+            this.heldItem?.setPosition(this.player.x, this.player.y - 50);
+        }
+    }
+    interactWithStove() {
+        if (
+            this.input.mousePointer.leftButtonDown() &&
+            this.player_arms.stoveOverlap &&
+            this.stove.inStove.length > 0 &&
+            !this.mouseClicked &&
+            !this.heldItem
+        ) {
+            this.itemGroup?.add(this.stove.makeDish());
+        }
+    }
+
     update() {
         //Movement
         this.player.setVelocity(0);
         this.player_arms.setVelocity(0);
         this.player.movePlayer(this.player_arms);
 
-        //GrabObjects function and associated math.
+        //Left mouse button is down
+        const leftButtonDown = this.input.mousePointer.leftButtonDown();
+
+        //Player is not holding an item.
         if (!this.player_arms.hasItem) {
-            // If player_arms is not holding an item.
             if (
-                this.input.mousePointer.leftButtonDown() &&
+                leftButtonDown &&
                 this.player_arms.overlapping &&
                 !this.mouseClicked
             ) {
-                //If LMB is clicked, and arms are overlapping, grab item.
-                this.player_arms.hasItem = true;
-                this.player_arms.flipY = true;
-                this.player_arms.anims.play("grab");
-                //Position item above the player.
-                this.heldItem?.setPosition(this.player.x, this.player.y - 50);
-                this.mouseClicked = true;
+                this.pickupItem(true);
             } else {
-                //If no item is grabbed/LMB not pressed
-                this.player_arms.flipY = false;
-                this.player_arms.anims.play("idle");
+                this.player_arms.playAnims(false);
+                //Check to see if player clicked to interact with stove.
+                this.interactWithStove();
             }
         } else {
-            //If holding item
-            this.player_arms.flipY = true;
-            this.player_arms.anims.play("grab");
-            //Position above head.
-            this.heldItem?.setPosition(this.player.x, this.player.y - 50);
-            //If LMB released while holding item, drop it
+            //Player is holding an item.
+            this.pickupItem(false);
+
+            //If statement that checks if overlapping with stove.
             if (
-                this.input.mousePointer.leftButtonDown() &&
+                leftButtonDown &&
+                this.player_arms.stoveOverlap &&
+                this.heldItem &&
+                !this.mouseClicked
+            ) {
+                //Disable rendering of item, put in stove, prevent from being able to be interacted with.
+                this.heldItem.setPosition(3000, 3000); //send item offscreen for now, will delete later
+                this.stove.insertItem(this.heldItem);
+                this.heldItem.disableBody(true, true);
+                //Clear referenced item, set hasItem to false.
+                this.heldItem = null;
+                this.player_arms.hasItem = false;
+                this.mouseClicked = true;
+            } else if (
+                leftButtonDown &&
+                !this.player_arms.stoveOverlap &&
                 !this.mouseClicked
             ) {
                 this.heldItem?.setY(this.player.y + 10);
@@ -139,11 +227,7 @@ export default class day1 extends Phaser.Scene {
                 this.mouseClicked = true;
             }
         }
-        //Simple mouseclick check
-        if (!this.input.mousePointer.leftButtonDown()) {
-            this.mouseClicked = false;
-        }
-        this.player_arms.overlapping = false;
+        //Reset clicked boolean
+        this.resetClicked();
     }
-
 }
